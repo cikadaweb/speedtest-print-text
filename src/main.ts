5715cead271f6e532430863c0e7b7ca14984b343
cikada <adel.garipovazn@mail.ru>
2023-04-27 21:32:25 +0300

    Add bootstrap & icons

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.css"
+
 const app = createApp(App)
 
 app.use(createPinia())
