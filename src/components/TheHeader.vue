9edffff05462d094e1013988bf6ccedb168e17d7
cikada <adel.garipovazn@mail.ru>
2023-04-30 22:51:30 +0300

    Craete Header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
-defineProps<
-{
-}
->()
+
 </script>
 
 <template>
-  <div>
-    Header
+  <div class="container pt-1">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-success bg-gradient text-white navbar-dark rounded-3">
+      <div class="container-fluid">
+        <a class="navbar-brand fs-3 fw-bold text-uppercase" href="#">Speed Test Text</a>
+        <button class="navbar-toggler"   data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <button type="button" class="btn btn-link text-white fw-bold text-uppercase text-decoration-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Об авторе
+            </button>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
+
 </template>
 
 <style scoped>
