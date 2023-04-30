2c300053bf437e5600d32cd823ab2d4518537e1a
cikada <adel.garipovazn@mail.ru>
2023-04-30 22:50:48 +0300

    Create AppModal

---
+++ src/components/AppModal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Обо мне</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body fs-5">
+          <div>Профиль на
+            <a class="text-black" href="https://github.com/cikadaweb" target="_blank"><i class="bi bi-github"></i> GitHub</a>
+          </div>
+          <div>Связаться
+            <a class="text-black" href="mailto:adel.garipovazn@mail.ru" target="_blank">Mail <i class="bi bi-envelope-at-fill"></i></a>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Закрыть</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
