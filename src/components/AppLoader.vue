51249a15992ea3deb9e4be4baf50864ce9c275be
cikada <adel.garipovazn@mail.ru>
2023-04-30 22:14:56 +0300

    Create AppLoader.vue

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,17 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="laoder spinner-grow text-success mt-5" role="status">
+    <span class="sr-only"></span>
+  </div>
+</template>
+
+<style scoped>
+.laoder {
+  height: 50px;
+  width: 50px;
+}
+</style>
