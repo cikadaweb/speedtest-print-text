497c7fdaa6b86f041d724a2153249aabb3f597fd
cikada <adel.garipovazn@mail.ru>
2023-05-02 00:27:16 +0300

    Create type TextSpeed

---
+++ src/components/AppTextSpeed.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { toRefs, computed } from "vue";
+
+const props = defineProps<{
+  timePassed: number,
+  correctTapCount: number
+}>();
+
+const { timePassed, correctTapCount } = toRefs(props);
+
+const speedResult = computed(() => {
+  if (Math.ceil((correctTapCount.value / timePassed.value) * 60).toString() === 'NaN') {
+    return 0;
+  } else {
+    return Math.ceil((correctTapCount.value / timePassed.value) * 60);
+  }
+});
+</script>
+
+<template>
+  <div class="text-speed fs-4 fw-bold text-uppercase">
+    <div class="opacity-75">
+      <i class="bi bi-speedometer"></i> Скорость
+    </div>
+    <div class="mt-2">
+      <span class="text-speed__counter fs-3">{{ speedResult }}</span> ЗН./МИН
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
