cbd42db30ce3ebedefb0b0e035ebbdb9974edbf0
cikada <adel.garipovazn@mail.ru>
2023-05-02 00:27:32 +0300

    [WIP] Create Main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,52 +2,93 @@
 import AppTextArea from '@/components/AppTextArea.vue';
 import AppTextSpeed from '@/components/AppTextSpeed.vue';
 import AppTextResult from '@/components/AppTextResult.vue';
-import { ref } from "vue";
+import AppLoader from '@/components/AppLoader.vue';
+
+import { ref, onBeforeUnmount } from "vue";
 
 const text = ref('');
-const isResetCounter = ref(false);
+
+const isShowLoader = ref(true);
+const isShowFinishModal = ref(false);
 
 const fetchApiText = async () => {
+  isShowLoader.value = true;
   const response = await fetch(
-    "https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1"
+    "https://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=1"
   );
   const data = await response.json();
-  text.value = data.join("");
-  isResetCounter.value = true;
+  text.value = data.join("").replace(/  +/g, " ");;
+  isShowLoader.value = false;
 }
 
 const changeResetStatus = () => {
-  isResetCounter.value = false;
+  correctTapCount.value = 0;
+  inсorrectTapCount.value = 0;
+  timePassed.value = 0;
 }
 
 fetchApiText();
+
+const correctTapCount = ref(0); // кол-во правильно введенных символов
+const inсorrectTapCount = ref(0); // кол-во неправильно введенных символов
+
+const incrCorrectTapCount = () => {
+  correctTapCount.value++;
+};
+
+const incrIncorrectTapCount = () => {
+  inсorrectTapCount.value++;
+};
+
+const timePassed = ref(0);
+
+const interval = setInterval(() => {
+  timePassed.value++;
+}, 1000);
+
+onBeforeUnmount(() => {
+  clearInterval(interval);
+});
 
 </script>
 
 <template>
   <div class="container d-flex justify-content-center mt-5">
-    <div class="card px-3 py-3 border-0 mb-3" style="max-width: 1200px;">
+    
+    <AppLoader v-if="isShowLoader"/>
+
+    <div v-else class="card px-3 py-3 border-0 mb-3" style="max-width: 1200px;">
       <div class="row g-0">
         <div class="col-md-8">
 
-          <AppTextArea :text="text" :isResetCounter="isResetCounter" @changeResetStatus="changeResetStatus" />
+        <AppTextArea
+          :text="text"
+          @change-reset-status="changeResetStatus"
+          @incr-correct-tap-count="incrCorrectTapCount"
+          @incr-incorrect-tap-count="incrIncorrectTapCount"
+        />
 
         </div>
         <div class="col-md-4">
           <div class="card h-100 border-0">
             <div class="card-body d-flex flex-column justify-content-between h-100 px-3 py-3">
 
-              <div class="card-body__info">
-                <AppTextSpeed />
-                <AppTextResult />
+              <div class="card-body__info text-center text-md-start">
+                <AppTextSpeed :time-passed="timePassed" :correct-tap-count="correctTapCount" />
+                <AppTextResult :inсorrect-tap-count="inсorrectTapCount" :correct-tap-count="correctTapCount" :text="text"/>
               </div>
 
-              <button class="btn btn-link fs-5 fw-bold text-uppercase text-decoration-none text-start text-success"
-                type="button" @click="fetchApiText"><i class="bi bi-arrow-clockwise"></i> Заново</button>
+              <button 
+                class="btn btn-link fs-5 fw-bold text-uppercase text-decoration-none text-start text-success text-center text-md-start mt-5 mt-sm-3"
+                type="button" 
+                @click="fetchApiText">
+                <i class="bi bi-arrow-clockwise me-1"></i>Заново
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
